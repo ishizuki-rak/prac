52cdc9c338b14225e704f64c35d730fec273f49e
CON-WND-0017\connect <ryoma.ishizuki@connec10.co.jp>
2021-05-20 16:26:45 +0900

    cssがお粗末。vueを使ってないので、可変対応する。demoで見れる。

---
+++ backend/pract/public/js/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <Header></Header>
+    <Nuxt/>
+    <Footer></Footer>
+</template>
+
+<script>
+import Header from '/header'
+import Footer from '/footer'
+export default {
+    components: {
+        Header,
+        Footer
+    },
+    mount(){
+        this.$api.get('auth').then(function() {
+            console.log('初期化Auth認証（仮）')
+        })
+    }
+}
+</script>
---
+++ backend/pract/public/js/pages/index.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="main">
+        <div class="user-info">
+            <User v-if="user" v-model="user" @click="route('/detail/' + user.id)">
+        </div>
+        <div class="local">
+            <div class="circle-button">
+                <div class="button-title">Go</div>
+                <div class="button-content">challenge!!</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            user: {
+                id: 1,
+                name: 'name01', 
+                pt: 1901, 
+                gauge: 1, 
+            }
+        }
+    },
+    mount() {
+        console.log('mount')
+    },
+    created() {
+        this.$api.get('top').then(function(res) {
+            console.log(res)
+
+        })
+    },
+    methods: {
+        route(url) {
+            this.$router.push(url)
+        },
+        challenge() {
+            if(locateFlg){
+                
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+    .main {
+        padding: 0;
+        margin: 0;
+        border: none;
+        font-size: 16px;
+        font-weight: 300;
+        color:black;
+        background: white;
+        overflow: hidden;
+        width: 100%;
+        height: 100%;
+    }
+    .circle-button {
+        text-align: center;
+        margin: 0 auto;
+        width: 70vw;
+        height: 70vw;
+        border: solid black 2px;
+        box-shadow: 5px 5px 5px 5px;
+        background: linear-gradient(pink, red);
+    }
+    .button-title {
+        font-size: 1.6em;
+        font-weight: bold;
+    }
+    .button-content {
+        font-size: 1.2em;
+        font-weight: bold;
+    }
+</style>
